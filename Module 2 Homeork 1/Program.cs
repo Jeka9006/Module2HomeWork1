1e472c552066a3b26d5d5da56d62fc9829444d28
DESKTOP-9C62S0S\umn90 <umn90jeka@gmail.com>
2021-02-16 11:15:31 +0200

    program

---
+++ Module 2 Homeork 1/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace HomeWork
+{
+    /// <summary>
+    /// Start point of app.
+    /// </summary>
+    public class Program
+    {/// <summary>
+     /// Start of app.
+     /// </summary>
+     /// <param name="args">args added via console.</param>
+        public static void Main(string[] args)
+        {
+           System.Console.WriteLine("888");
+        }
+    }
+}
